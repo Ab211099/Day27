ad19fdb13b287c0e84e2f26d0f9b6b75a5c61faa
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-10-03 16:33:17 +0530

    [Abhi]Added:Ability to read and write from Text file

---
+++ FileIO/AddressBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    public class AddressBook
+    {
+        private static string path = @"C:\Users\Administrator\Desktop\Day27\FileIO\FileIO.txt";
+        public static void ReadFromStreamReader()
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = " ";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public static void WriteUsingStreamReader()
+        {
+            
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                Console.WriteLine();
+                sr.WriteLine(" Firstname = Suresh, Lastname = Gaikwad, Contact number = 844614462, ZipCode = 411025 ");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
---
+++ FileIO/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //AddressBook.ReadFromStreamReader();
+            AddressBook.WriteUsingStreamReader();
+        }
+    }
+}
